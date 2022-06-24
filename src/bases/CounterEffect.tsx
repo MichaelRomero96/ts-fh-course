801e82b86d4c5abf13da392de39f3ec0939d4e3c
MichaelRomero96 <michael.romero@meltstudio.co>
2022-06-24 12:30:23 -0500

    Animating with gsap async library

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { gsap } from 'gsap';
 
 export default function CounterEffect() {
     const MAXIMUM_COUNT = 10;
@@ -16,11 +17,17 @@ export default function CounterEffect() {
     useEffect(() => {
         if (counter < 10) return;
         console.log('%cSe llego al valor máximo', consoleStyle);
+
+        gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
+            gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' })
+        })
+
     }, [counter, consoleStyle])
 
     return (
         <>
-            <h1>CounterEffect: {counter}</h1>
+            <h1>CounterEffect:</h1>
+            <h2>{counter}</h2>
             <button onClick={handleClick}>
                 +1
             </button>
